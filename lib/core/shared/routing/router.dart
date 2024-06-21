33a021a437c0e5e0c37232fa62ce0a3e9453cc48
mlg2034 <ashatkabytaev@gmail.com>
2024-06-21 21:51:58 +0500

    added nested navigation

--- lib/core/shared/routing/router.dart
+++ lib/core/shared/routing/router.dart
@@ -1,16 +1,55 @@
 import 'package:admin_panel/features/main_screen/screen/main_screen.dart';
+import 'package:admin_panel/features/navigation_rail/screen/navigation_rail_screen.dart';
+import 'package:admin_panel/features/notifications/screen/notification_screen.dart';
+import 'package:admin_panel/features/settings/screen/settings_screen.dart';
+import 'package:admin_panel/features/users/screen/users_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class AppRouter {
-  final BuildContext context;
-  AppRouter({required this.context});
-  final router = GoRouter(
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const MainScreen(),
-      )
-    ],
-  );
-}
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
+final router = GoRouter(
+  navigatorKey: _rootNavigatorKey,
+  routes: [
+    ShellRoute(
+      navigatorKey: _shellNavigatorKey,
+      builder: (_, GoRouterState state, child) =>
+          const NavigationRailScreen(),
+      // pageBuilder: (context, state, child) {
+      //   return CustomTransitionPage(
+      //     key: state.pageKey,
+      //     child: child,
+      //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      //       return FadeTransition(
+      //         opacity:
+      //             CurveTween(curve: Curves.easeInOutCirc).animate(animation),
+      //         child: child,
+      //       );
+      //     },
+      //   );
+      // },
+      routes: [
+        GoRoute(
+          parentNavigatorKey: _shellNavigatorKey,
+          path: '/',
+          builder: (_, __) => const MainScreen(),
+        ),
+        GoRoute(
+          parentNavigatorKey: _shellNavigatorKey,
+          path: '/users',
+          builder: (_, __) => const UsersScreen(),
+        ),
+        GoRoute(
+          parentNavigatorKey: _shellNavigatorKey,
+          path: '/settings',
+          builder: (_, __) => const SettingsScreen(),
+        ),
+        GoRoute(
+          parentNavigatorKey: _shellNavigatorKey,
+          path: '/notifications',
+          builder: (_, __) => const NotificationScreen(),
+        ),
+      ],
+    ),
+  ],
+);
---
+++ lib/src/ui/theme/colors_sheme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomColorSheme {
+ 
+  static const ColorScheme lightColorScheme = ColorScheme(
+    primary: Color.fromARGB(255, 48, 48, 72),
+    onPrimary: Colors.black,
+    secondary: Color(0xFFEFF3F3),
+    onSecondary: Color(0xFF322942),
+    error: Colors.redAccent,
+    onError: Colors.white,
+    surface: Color(0xFFFAFBFB),
+    onSurface: Color(0xFF241E30),
+    brightness: Brightness.light,
+  );
+  static const ColorScheme darkColorScheme = ColorScheme(
+    primary: Color(0xFFFF8383),
+    secondary: Color(0xFF4D1F7C),
+    surface: Color(0xFF1F1929),
+    error: Colors.redAccent,
+    onError: Colors.white,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    onSurface: Colors.white,
+    brightness: Brightness.dark,
+  );
+}
